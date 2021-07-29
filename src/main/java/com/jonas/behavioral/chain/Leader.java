42bc7a7164aee53fa94e678021eccfe1c9a591c3
shenjy <shenjy24@gmail.com>
2021-07-29 17:43:40 +0800

    责任链模式

---
+++ src/main/java/com/jonas/behavioral/chain/Leader.java
@@ -0,0 +1,19 @@
+package com.jonas.behavioral.chain;
+
+/**
+ * 抽象处理者：领导类
+ */
+public abstract class Leader {
+    private Leader next;
+
+    public void setNext(Leader next) {
+        this.next = next;
+    }
+
+    public Leader getNext() {
+        return next;
+    }
+
+    //处理请求的方法
+    public abstract void handleRequest(int LeaveDays);
+}
